890a9e227c71069a9f00ab65f97fe3170fd53bd8
mateohideg2 <mateohideg2@gmail.com>
2019-05-04 23:44:49 -0300

    Update SSLS.js

--- SSLS.js
+++ SSLS.js
@@ -1,8 +1,11 @@
+if(location.host === "scratch.mit.edu") { bookmarklet(); }
+function bookmarklet() { 
 var newDiv = document.createElement("div"); 
 newDiv.setAttribute("id", "buttonLeave");
 document.getElementById('buttonLeave').innerHTML = '<a class="button small grey" id="leave_studios"><span class="text">Leave Studios</span></a>';
 var currentDiv = document.getElementById("main-content"); 
 document.body.insertBefore(newDiv, currentDiv); 
+}
 
 var elementToClick = document.getElementById("leave_studios");
 elementToClick.addEventListener("click", removeCurator);
@@ -11,4 +14,3 @@ function removeCurator() {
 var studioid = prompt("Write the studio ID", "");
 $.ajax({type: "PUT",url: "https://scratch.mit.edu/site-api/users/curators-in/" + studioid + "/remove/?usernames=" + Scratch.INIT_DATA.LOGGED_IN_USER.model.username});
 }
-if(location.host === "scratch.mit.edu") { bookmarklet(); }
